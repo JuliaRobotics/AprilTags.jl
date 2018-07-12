e4a18e6762f65a2899fa182795de6248c93cc07b
GearsAD <sclaassens.ad@gmail.com>
2018-07-12 12:05:28 -0500

    More visible box around tags

--- src/tagdraw.jl
+++ src/tagdraw.jl
@@ -6,14 +6,30 @@ Draw a box around the tag.
 foreach(tag->drawTagBox!(imageCol, tag), tags)
 `
 """
-function drawTagBox!(image::Array{T,2}, tag::AprilTag) where T <: RGB
-
+function drawTagBox!(image::Array{T,2}, tag::AprilTag, width=1) where T <: RGB
     cpoints = map(tag -> Point(round.(Int,tag)...),tag.p)
-      # Make a box around the tag
-    draw!(image, LineSegment( cpoints[1], cpoints[2]), RGB{N0f8}(0.0, 0.0, 1.0))
-    draw!(image, LineSegment( cpoints[2], cpoints[3]), RGB{N0f8}(0.0, 0.0, 1.0))
-    draw!(image, LineSegment( cpoints[3], cpoints[4]), RGB{N0f8}(1.0, 0.0, 0.0))
-    draw!(image, LineSegment( cpoints[4], cpoints[1]), RGB{N0f8}(0.0, 1.0, 0.0))
+    # Reticle
+    midx = (cpoints[3].x + cpoints[1].x) / 2
+    midy = (cpoints[3].y + cpoints[1].y) / 2
+    x1 = Point(round(midx - (cpoints[2].x - cpoints[1].x) / 4), round(midy - (cpoints[2].y - cpoints[1].y) / 4))
+    x2 = Point(round(midx + (cpoints[2].x - cpoints[1].x) / 4), round(midy + (cpoints[2].y - cpoints[1].y) / 4))
+    y1 = Point(round(midx - (cpoints[4].x - cpoints[1].x) / 4), round(midy - (cpoints[4].y - cpoints[1].y) / 4))
+    y2 = Point(round(midx + (cpoints[4].x - cpoints[1].x) / 4), round(midy + (cpoints[4].y - cpoints[1].y) / 4))
+    draw!(image, LineSegment( x1, x2), RGB{N0f8}(1.0, 0.0, 0.0))
+    draw!(image, LineSegment( y1, y2), RGB{N0f8}(1.0, 0.0, 0.0))
+
+    # Make a box around the tag
+    for i in 1:width
+        draw!(image, LineSegment( cpoints[1], cpoints[2]), RGB{N0f8}(0.0, 0.0, 1.0))
+        draw!(image, LineSegment( cpoints[2], cpoints[3]), RGB{N0f8}(0.0, 0.0, 1.0))
+        draw!(image, LineSegment( cpoints[3], cpoints[4]), RGB{N0f8}(1.0, 0.0, 0.0))
+        draw!(image, LineSegment( cpoints[4], cpoints[1]), RGB{N0f8}(0.0, 1.0, 0.0))
+        # Expand it
+        cpoints[1] = Point(cpoints[1].x-1, cpoints[1].y-1)
+        cpoints[2] = Point(cpoints[2].x+1, cpoints[2].y-1)
+        cpoints[3] = Point(cpoints[3].x+1, cpoints[3].y+1)
+        cpoints[4] = Point(cpoints[4].x-1, cpoints[4].y+1)
+    end
     return nothing
 end
 
