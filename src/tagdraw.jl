ee23b1d78e9d15b85e79866ab3c281f920b369c3
Johan <terblanche.j@gmail.com>
2018-07-13 14:28:21 +0200

    slight improvement on the way thicker lines are drawn plus basic tests

--- src/tagdraw.jl
+++ src/tagdraw.jl
@@ -6,33 +6,96 @@ Draw a box around the tag.
 foreach(tag->drawTagBox!(imageCol, tag), tags)
 `
 """
-function drawTagBox!(image::Array{T,2}, tag::AprilTag, width=1) where T <: RGB
+function drawTagBox!(image::Array{T,2}, tag::AprilTag; width::Int = 1, drawReticle::Bool = true) where T <: RGB
     cpoints = map(tag -> Point(round.(Int,tag)...),tag.p)
     # Reticle
-    midx = (cpoints[3].x + cpoints[1].x) / 2
-    midy = (cpoints[3].y + cpoints[1].y) / 2
-    x1 = Point(round(midx - (cpoints[2].x - cpoints[1].x) / 4), round(midy - (cpoints[2].y - cpoints[1].y) / 4))
-    x2 = Point(round(midx + (cpoints[2].x - cpoints[1].x) / 4), round(midy + (cpoints[2].y - cpoints[1].y) / 4))
-    y1 = Point(round(midx - (cpoints[4].x - cpoints[1].x) / 4), round(midy - (cpoints[4].y - cpoints[1].y) / 4))
-    y2 = Point(round(midx + (cpoints[4].x - cpoints[1].x) / 4), round(midy + (cpoints[4].y - cpoints[1].y) / 4))
-    draw!(image, LineSegment( x1, x2), RGB{N0f8}(1.0, 0.0, 0.0))
-    draw!(image, LineSegment( y1, y2), RGB{N0f8}(1.0, 0.0, 0.0))
-
+    if drawReticle
+        midx = (cpoints[3].x + cpoints[1].x) / 2
+        midy = (cpoints[3].y + cpoints[1].y) / 2
+        x1 = Point(round(midx - (cpoints[2].x - cpoints[1].x) / 4), round(midy - (cpoints[2].y - cpoints[1].y) / 4))
+        x2 = Point(round(midx + (cpoints[2].x - cpoints[1].x) / 4), round(midy + (cpoints[2].y - cpoints[1].y) / 4))
+        y1 = Point(round(midx - (cpoints[4].x - cpoints[1].x) / 4), round(midy - (cpoints[4].y - cpoints[1].y) / 4))
+        y2 = Point(round(midx + (cpoints[4].x - cpoints[1].x) / 4), round(midy + (cpoints[4].y - cpoints[1].y) / 4))
+        draw!(image, LineSegment( x1, x2), RGB{N0f8}(1.0, 0.0, 0.0))
+        draw!(image, LineSegment( y1, y2), RGB{N0f8}(1.0, 0.0, 0.0))
+    end
     # Make a box around the tag
-    for i in 1:width
+    if width <= 1
         draw!(image, LineSegment( cpoints[1], cpoints[2]), RGB{N0f8}(0.0, 0.0, 1.0))
         draw!(image, LineSegment( cpoints[2], cpoints[3]), RGB{N0f8}(0.0, 0.0, 1.0))
         draw!(image, LineSegment( cpoints[3], cpoints[4]), RGB{N0f8}(1.0, 0.0, 0.0))
         draw!(image, LineSegment( cpoints[4], cpoints[1]), RGB{N0f8}(0.0, 1.0, 0.0))
-        # Expand it
-        cpoints[1] = Point(cpoints[1].x-1, cpoints[1].y-1)
-        cpoints[2] = Point(cpoints[2].x+1, cpoints[2].y-1)
-        cpoints[3] = Point(cpoints[3].x+1, cpoints[3].y+1)
-        cpoints[4] = Point(cpoints[4].x-1, cpoints[4].y+1)
+    else
+        drawThickLine!(image, cpoints[1], cpoints[2], RGB{N0f8}(0.0, 0.0, 1.0), width)
+        drawThickLine!(image, cpoints[2], cpoints[3], RGB{N0f8}(0.0, 0.0, 1.0), width)
+        drawThickLine!(image, cpoints[3], cpoints[4], RGB{N0f8}(1.0, 0.0, 0.0), width)
+        drawThickLine!(image, cpoints[4], cpoints[1], RGB{N0f8}(0.0, 1.0, 0.0), width)
     end
+
     return nothing
 end
 
+function drawThickLine!(image, startpoint, endpoint, colour, thickness)
+    (row, col) = size(image)
+    x1 = startpoint.x
+    y1 = startpoint.y
+    x2 = endpoint.x
+    y2 = endpoint.y
+
+    draw!(image, LineSegment(x1,y1,x2,y2), colour)
+
+    if x1 != x2 && (y2-y1)/(x2-x1) < 1
+        for tn = 1:thickness
+            i = tn ÷ 2
+
+            x1mi = x1-i
+            x1pi = x1+i
+            y1mi = y1-i
+            y1pi = y1+i
+            x2mi = x2-i
+            x2pi = x2+i
+            y2mi = y2-i
+            y2pi = y2+i
+            #clip to protect bounds
+            (x1mi < 1) && (x1mi = 1)
+            (y1mi < 1) && (y1mi = 1)
+            (x1pi > col) && (x1pi = col)
+            (y1pi > row) && (y1pi = row)
+            (x2mi < 1) && (x2mi = 1)
+            (y2mi < 1) && (y2mi = 1)
+            (x2pi > col) && (x2pi = col)
+            (y2pi > row) && (y2pi = row)
+
+            iseven(tn) && draw!(image, LineSegment(x1,y1mi,x2,y2mi), colour)
+            isodd(tn)  && draw!(image, LineSegment(x1,y1pi,x2,y2pi), colour)
+        end
+    else
+        for tn = 1:thickness
+            i = tn ÷ 2
+
+            x1mi = x1-i
+            x1pi = x1+i
+            y1mi = y1-i
+            y1pi = y1+i
+            x2mi = x2-i
+            x2pi = x2+i
+            y2mi = y2-i
+            y2pi = y2+i
+            #clip to protect bounds
+            (x1mi < 1) && (x1mi = 1)
+            (y1mi < 1) && (y1mi = 1)
+            (x1pi > col) && (x1pi = col)
+            (y1pi > row) && (y1pi = row)
+            (x2mi < 1) && (x2mi = 1)
+            (y2mi < 1) && (y2mi = 1)
+            (x2pi > col) && (x2pi = col)
+            (y2pi > row) && (y2pi = row)
+
+            iseven(tn) && draw!(image, LineSegment(x1mi,y1,x2mi,y2), colour)
+            isodd(tn)  && draw!(image, LineSegment(x1pi,y1,x2pi,y2), colour)
+        end
+    end
+end
 
 """
     drawTagAxes!(image, tag, CameraMatrix)
@@ -65,6 +128,7 @@ function drawTagAxes!(image::Array{T,2}, tag::AprilTag, K::Array{Float64,2}) whe
     ip1 = Point(round.(Int,p1[1:2])...)
     ip2 = Point(round.(Int,p2[1:2])...)
     ip3 = Point(round.(Int,p3[1:2])...)
+    #TODO check that the line stays on the image to avoid bounds errors
     draw!(image, LineSegment(ip0, ip1), RGB{N0f8}(1.0, 0.0, 0.0))
     draw!(image, LineSegment(ip0, ip2), RGB{N0f8}(0.0, 1.0, 0.0))
     draw!(image, LineSegment(ip0, ip3), RGB{N0f8}(0.0, 0.0, 1.0))
